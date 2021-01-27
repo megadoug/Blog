7ebb93390be58d73b5afda87a0dc72307331840b
megadoug <megadoug@gmail.com>
2021-01-27 14:52:37 -0300

    Criação do Detail View e

--- Blog/models.py
+++ Blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='publicado')
 
 class Post(models.Model):
     STATUS = (
@@ -12,14 +16,20 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250)
     autor = models.ForeignKey(User,
                               on_delete=models.CASCADE)
-    conteudos = models.TextField()
+    conteudo = models.TextField()
     publicado = models.DateTimeField(default=timezone.now())
     criado = models.DateTimeField(auto_now_add=True)
     alterado = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
                               choices=STATUS,
                               default='rascunho')
+    
+    object = models.Manager()
+    published = PublishedManager()
 
+    def get_absolute_url(self):
+        # return reverse('post_detail',args=[self.pk])
+        return reverse('post_detail',args=[self.slug])
     class Meta:
         ordering = ('-publicado',)
 
--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
 
 # Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "Blog/home.html"
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "Blog/post_detail.html"
+    context_object_name = 'custom'
